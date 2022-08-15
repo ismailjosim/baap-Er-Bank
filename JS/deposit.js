a5f10c94e681b28d268d28fcf0db1a6cf6403bcd
ISMAIL JOSIM <ismailjosim>
2022-08-16 01:16:13 +0600

    widthdraw code added

--- JS/deposit.js
+++ JS/deposit.js
@@ -5,12 +5,39 @@ const depositBalance = document.getElementById('deposit-balance');
 
 const totalBalance = document.getElementById('total-balance');
 
-
-
-
+const WithdrawalAmount = document.getElementById('withdrawal-field');
+const WithdrawalButton = document.getElementById('btn-withdrawal');
+const withdrawalBalance = document.getElementById('withdrawal-balance');
 
 
 // Event
+// Withdrawal Amount section
+// deposit Amount section
+WithdrawalButton.addEventListener('click', function () {
+    // Step-01: get input amount
+    const NewAmountStr = WithdrawalAmount.value;
+    const NewAmount = parseFloat(NewAmountStr);
+
+    // step-02: Get deposit balance Amount
+    const PreviousBalanceStr = withdrawalBalance.innerText;
+    const PreviousBalance = parseFloat(PreviousBalanceStr);
+
+    // step-03: input + balance amount
+    const deposit = PreviousBalance + NewAmount;
+    withdrawalBalance.innerText = PreviousBalance + NewAmount;
+
+    // step-04: Convert total amount to number
+    const Balance = totalBalance.innerText;
+    const mainBalance = parseFloat(Balance);
+
+    // step-05: update main balance
+    totalBalance.innerText = mainBalance - NewAmount;
+
+    // step-06: clear input field
+    WithdrawalAmount.value = '';
+});
+
+// deposit Amount section
 depositButton.addEventListener('click', function () {
     // Step-01: get input amount
     const NewAmountStr = depositAmount.value;
@@ -34,3 +61,5 @@ depositButton.addEventListener('click', function () {
     // step-06: clear input field
     depositAmount.value = '';
 });
+
+// Withdrawal Amount section
